a18a9824518a01a2cbd06d3c3994e47927b81697
Sebastian Echeverri <sebas.eche98@gmail.com>
2025-11-10 20:40:57 -0500

    Set budget component created

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,40 @@
+import { useMemo, useState, type ChangeEvent } from "react"
+
+export default function BudgetForm() {
+
+    const [budget, setBudget] = useState(0)
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+
+    return (
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                    Set Budget
+                </label>
+                <input
+                    id="budget"
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2 rounded-lg [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                    placeholder="Set your budget"
+                    name="budget"
+                    value={budget > 0 ? budget : ''}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input
+                type="submit"
+                value="Set Budget"
+                className="bg-blue-600 cursor-pointer w-full p-2 text-white font-bold uppercase tracking-wider transition-colors duration-300 disabled:opacity-40 disabled:cursor-not-allowed disabled:hover:bg-blue-600 hover:bg-blue-700 active:bg-blue-800"
+                disabled={isValid}
+            />
+        </form>
+    )
+}
