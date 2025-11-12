de802df0c3bd8fc0252dd70c9bf15623f553cdc9
Sebastian Echeverri <sebas.eche98@gmail.com>
2025-11-11 20:44:08 -0500

    currency format helper created

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers/formatCurrency"
 
 type AmountDisplayProps = {
     label: string,
@@ -8,7 +9,7 @@ export default function AmountDisplay({ label, amount }: AmountDisplayProps) {
     return (
         <p className="text-2xl text-blue-600 font-bold">
             {label}: {''}
-            <span className="font-black">{amount}</span>
+            <span className="font-black text-black">{formatCurrency(amount)}</span>
         </p>
     )
 }
